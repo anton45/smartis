e94181e58ed7f64c41e82e6ab35d1d4815e9e717
anton goleusov <antongoleusov@gmail.com>
2022-04-29 05:03:39 +0300

    last

--- index.php
+++ index.php
@@ -9,23 +9,6 @@ $postData = $_POST;
 $controller = new ExchangeRateController($postData);
 $result = $controller->main($postData);
 
-//    $buildurl = ('https://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=' . $startDate . '&date_req2=' . $stopDate . '&VAL_NM_RQ=' . $currency);
-//    $xmlString = file_get_contents($buildurl);
-//    $xml = simplexml_load_string($xmlString, "SimpleXMLElement", LIBXML_NOCDATA);
-//    $json = json_encode($xml);
-//    $array = json_decode($json,TRUE);
-//
-//$settings = $array["@attributes"];
-//unset($array["@attributes"]);
-//foreach ($array as $value) {
-//    foreach ($value as $element) {
-//        $newarray[] = (int)$element["Value"];
-//    }
-//}
-//$json = json_encode($newarray);
-
-
-
 
 ?>
 <html>
